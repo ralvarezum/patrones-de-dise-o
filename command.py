cf4c1ecfef4a65d12c932fd029b3fb7ef5e9863e
Rodrigo <rorototi@hotmail.com>
2023-10-30 15:36:29 -0300

    Cambios en command

--- command.py
+++ command.py
@@ -2,28 +2,47 @@ class Command:
     def execute(self):
         pass
 
-class LightOnCommand(Command):
-    def __init__(self, light):
-        self.light = light
+class RestartCommand(Command):
+    def __init__(self, computer):
+        self.computer = computer
 
     def execute(self):
-        self.light.turn_on()
+        self.computer.restart()
 
-class Light:
-    def turn_on(self):
-        print("La luz está encendida.")
+class ShutdownCommand(Command):
+    def __init__(self, computer):
+        self.computer = computer
+
+    def execute(self):
+        self.computer.shutdown()
+
+class Computer:
+    def restart(self):
+        print("Reiniciando la computadora...")
+
+    def shutdown(self):
+        print("Apagando la computadora...")
 
 class RemoteControl:
+    def __init__(self):
+        self.command = None
+
     def set_command(self, command):
         self.command = command
 
     def press_button(self):
-        self.command.execute()
+        if self.command:
+            self.command.execute()
+        else:
+            print("No se ha configurado ningún comando")
 
-light = Light()
-light_on = LightOnCommand(light)
+computer = Computer()
+restart_command = RestartCommand(computer)
+shutdown_command = ShutdownCommand(computer)
 remote = RemoteControl()
-remote.set_command(light_on)
 
+remote.set_command(restart_command)
 remote.press_button()
 
+remote.set_command(shutdown_command)
+remote.press_button()
